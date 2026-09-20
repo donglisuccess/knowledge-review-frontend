ecb26351f2a31fcef27d12a8ccab5c736687b5a5
dongli5 <dongli5@lenovo.com>
2026-09-20 11:18:45 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ const {
   <div class="page">
     <header class="page-header">
       <h1>📚 Day Day Up</h1>
-      <p class="subtitle">Vue3 / TypeScript / JavaScript / 工程化 前端知识巩固</p>
+      <p class="subtitle">Vue3 / TypeScript / JavaScript / 工程化 前端知识巩固，备战全能型人才</p>
     </header>
 
     <main class="page-main">
